dc2730bbb7c614e994aee4bcb5fd33cc002ac605
Ram <quadrupleslap@gmail.com>
2017-08-18 17:45:23 +1000

    Redid chunky frame again.

--- src/chunky.rs
+++ src/chunky.rs
@@ -11,11 +11,7 @@ use super::{Image, AsBytes};
 /// store the RGBA values of the top-left pixel, then each of the RGBA values of
 /// the pixel immediately to the right, and so on, moving down through each row.
 #[derive(Clone, Debug)]
-pub struct Chunky<T, V = Vec<u8>>
-where
-    T: AsBytes,
-    V: AsRef<[u8]>
-{
+pub struct Chunky<T, V = Vec<u8>> {
     bytes: V,
     width: usize,
     height: usize,
@@ -46,22 +42,18 @@ where
     }
 }
 
-impl<T, V> Chunky<T, V>
-where
-    T: AsBytes,
-    V: AsRef<[u8]>
-{
+impl<T, V> Chunky<T, V> {
     /// Creates a new frame backed by the provided byte source.
     ///
     /// # Panics
     ///
     /// Panics if the length of the buffer is not
     /// `width * height * bytes_per_pixel`.
-    pub fn from_bytes(
-        width: usize,
-        height: usize,
-        bytes: V
-    ) -> Self {
+    pub fn from_bytes(width: usize, height: usize, bytes: V) -> Self
+    where
+        T: AsBytes,
+        V: AsRef<[u8]>
+    {
         assert_eq!(bytes.as_ref().len(), width * height * T::width());
         Self { bytes, width, height, pixel: PhantomData }
     }
@@ -75,37 +67,37 @@ where
     pub fn into_bytes(self) -> V {
         self.bytes
     }
-}
 
-impl<T, V> Chunky<T, V>
-where
-    T: AsBytes,
-    V: AsRef<[u8]> + AsMut<[u8]>
-{
     /// Returns a mutable view into the frame's byte source.
-    pub fn bytes_mut(&mut self) -> &mut [u8] {
+    pub fn bytes_mut(&mut self) -> &mut [u8]
+    where
+        V: AsMut<[u8]>
+    {
         self.bytes.as_mut()
     }
-}
 
-impl<T> Chunky<T> where T: AsBytes {
-    /// Creates a new frame using the given frame to fill the buffer.
-    /// It is guaranteed that the mapping will be called **exactly once** for
-    /// each of the integers in the range `[0, width) * [0, height)`.
-    pub fn new<U>(frame: U) -> Self
-    where U: Image<Pixel = T> + Sync {
-        let (width, height) = (frame.width(), frame.height());
-        let length = width * height;
-        let size = T::width() * length;
+    /// Set the frame's contents to that of the given frame.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the width and height of the given frame are not exactly the
+    /// same as the width and height of the chunky frame.
+    pub fn copy_from<U>(&mut self, frame: U)
+    where
+        T: AsBytes,
+        U: Image<Pixel = T> + Sync,
+        V: AsMut<[u8]>
+    {
+        assert_eq!(frame.width(), self.width);
+        assert_eq!(frame.height(), self.height);
 
-        let mut bytes = Vec::with_capacity(size);
-        let ptr = AtomicPtr::new(bytes.as_mut_ptr());
-        unsafe { bytes.set_len(size); }
+        let length = self.width * self.height;
+        let ptr = AtomicPtr::new(self.bytes.as_mut().as_mut_ptr());
+        let width = self.width;
 
         (0..length).into_par_iter().for_each(|i| unsafe {
             let ptr = ptr.load(Ordering::Relaxed);
             let (x, y) = (i % width, i / width);
-
             let bytes = T::Bytes::from(frame.pixel(x, y).into());
 
             ptr::copy_nonoverlapping(
@@ -114,8 +106,27 @@ impl<T> Chunky<T> where T: AsBytes {
                 T::width()
             );
         });
+    }
+}
 
-        Self::from_bytes(width, height, bytes)
+impl<T> Chunky<T> where T: AsBytes {
+    /// Creates a new frame using the given frame to fill the buffer.
+    /// It is guaranteed that the mapping will be called **exactly once** for
+    /// each of the integers in the range `[0, width) * [0, height)`.
+    pub fn new<U>(frame: U) -> Self
+    where
+        U: Image<Pixel = T> + Sync
+    {
+        let (width, height) = (frame.width(), frame.height());
+        let length = width * height;
+        let size = T::width() * length;
+
+        let mut bytes = Vec::with_capacity(size);
+        unsafe { bytes.set_len(size); }
+
+        let mut chunky = Self::from_bytes(width, height, bytes);
+        chunky.copy_from(frame);
+        chunky
     }
 }
 
